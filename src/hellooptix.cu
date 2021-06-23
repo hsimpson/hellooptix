9eeb46745d6f9f2742d7fa005f6c1da4dbe67834
Daniel Toplak <homer.simpson123@gmx.de>
2021-06-23 23:45:32 +0200

    feat: create pseudo random number generator

--- src/hellooptix.cu
+++ src/hellooptix.cu
@@ -7,8 +7,32 @@ extern "C" {
 __constant__ Params params;
 }
 
+__device__ float vdot(float2 v1, float2 v2) {
+  return (v1.x * v2.x) + (v1.y * v2.y);
+}
+
+__device__ float fract(float f) {
+  return f - floor(f);
+}
+
+__device__ float rand(float2 co) {
+  return fract(sinf(vdot(co, make_float2(12.9898f, 78.233f))) * 43758.5453f);
+}
+
 extern "C" __global__ void __raygen__draw_color() {
-  uint3       launch_index                                          = optixGetLaunchIndex();
-  RayGenData* rtData                                                = (RayGenData*)optixGetSbtDataPointer();
-  params.image[launch_index.y * params.imageWidth + launch_index.x] = make_uchar4(255u, 0, 0u, 255u);
+  uint3       launch_index = optixGetLaunchIndex();
+  RayGenData* rtData       = (RayGenData*)optixGetSbtDataPointer();
+
+  float x = (float)launch_index.x;
+  float y = (float)launch_index.y;
+
+  float r = rand(make_float2(x + 1, y + 1));
+  float g = rand(make_float2(x + 2, y + 2));
+  float b = rand(make_float2(x + 3, y + 3));
+
+  params.image[launch_index.y * params.imageWidth + launch_index.x] = make_uchar4(
+      r * 255u,
+      g * 255u,
+      b * 255u,
+      255u);
 }
